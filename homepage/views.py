57172761cb3dc998ce66a27af705dc07a23b832d
Iamthemarcin <Iamthemarcin@wp.pl>
2024-02-12 18:14:49 +0100

    back from germany work yay

--- homepage/views.py
+++ homepage/views.py
@@ -3,12 +3,9 @@ from django.http import HttpResponse
 from PIL import Image
 import requests
 import os
+import random
 from io import BytesIO
 from django.conf import settings
-
-
-# Create your views here.
-
 
 def index(request):
     headers = {
@@ -17,6 +14,9 @@ def index(request):
     cookies = {
         'cookie_name': 'cookie_value'
     }
+    game_modes = ['Brawl-Ball', 'Gem-Grab', 'Heist', 'Knockout', 'Wipeout', 'Hot-Zone']
+    chosen_mode = random.choice(game_modes)
+
     path = '{}/images/brawlers/'.format(settings.STATICFILES_DIRS[0])
     img_list = os.listdir(path)
     half = len(img_list)//2
@@ -24,22 +24,34 @@ def index(request):
     bottom_row = img_list[half:]
     events_request = requests.get('https://api.brawlapi.com/v1/events')
     events_json = events_request.json()
-    gamemode_icons = []
-    def print_values(data, search_word):
+    mode_icon_link = []
+
+
+    act_events_req = requests.get('https://api.brawlstars.com/v1/#/definitions/PowerPlaySeason')
+    act_events = act_events_req.json()
+    print(act_events)
+    def print_values(data, search_word, chosen_mode):        
         if isinstance(data,dict):
             for key,value in data.items():
-                if search_word == key and 'gamemode' in value and value not in gamemode_icons:
-                    gamemode_icons.append(value)
+                if search_word == key and chosen_mode in value and 'header' not in value: #aight i tried returning it but it would require me to use the yield generator stuff and then iterate over the result??? there has to be a better way of returning it without using a list xd
+                    mode_icon_link.append(value)
+                    break
                 if isinstance(value, dict) or isinstance(value, list):
-                    print_values(value, search_word)
+                    print_values(value, search_word, chosen_mode)
         if isinstance(data, list):
             for i in data:
-                print_values(i, search_word)
-    print_values(events_json, 'imageUrl')
-    # update_brawler_pics(request)
-    context = {'top_row':top_row, 'bottom_row':bottom_row}
+                print_values(i, search_word, chosen_mode)
+    print_values(events_json, 'imageUrl', chosen_mode)
+    print(mode_icon_link[0])
+    chosen_mode = chosen_mode.replace('-',' ')
+
+
+    # update_brawler_pics(request) #everytime you want to update the brawler run this, for now xd maybe later make this run every week or something
+    context = {'top_row':top_row, 'bottom_row':bottom_row, 'mode_icon_link' : mode_icon_link[0], 'chosen_mode': chosen_mode}
     return render(request, "homepage.html", context)
 
+
+#For now i just get the pics from the brawlify api whenever there's a big update
 def update_brawler_pics(request):
     all_brawlers_request = requests.get('https://api.brawlapi.com/v1/brawlers')
     all_brawlers_json = all_brawlers_request.json()
--- static/js/main.js
+++ static/js/main.js
@@ -21,7 +21,7 @@ function brawler_search(){
     const input_value = e.target.value.toLowerCase()
     Array.from(brawlers).forEach(function (brawler) {
       brawler.classList.add("hide")
-      brawler_name = brawler.id.toLowerCase().slice(0,-4) // the names have .png at the end cuz i just use data from some random api
+      brawler_name = brawler.id.toLowerCase().slice(0,-4) // the names have .png at the end cuz i just use data from brawlify api, im SURE ill change that later :)
       if (brawler_name.includes(input_value)){
         brawler.classList.remove("hide")
       }
@@ -30,19 +30,24 @@ function brawler_search(){
 }
 brawler_search()
 
+
 pick_number = 1
 function choose_brawler(brawler){
+  //check if the brawler has been picked, if not add the picked class
+  if (brawler.classList.contains("brawler-picked")){return}
   //get the next player box in order that doesnt have the picked class and then add the image of the picked brawler to it
   const picked_brawler_img_src = brawler.src
-  player_box = document.querySelector(".pick-image:not(.picked)#p" + pick_number);
+  player_box = document.querySelector(".pick-image:not(.pick-box-picked)#p" + pick_number);
   pick_number++
   if(player_box){
+    brawler.classList.add("brawler-picked")
+    brawler.style.opacity = 0.35 //signal that the brawler has been picked
     player_pick_image = player_box.children[0]
     let width = player_pick_image.width;
     let height = player_pick_image.height;
     // add picked class since we picked a brawler for da box
-    player_box.classList.add("picked")
-    // cant just switch bg images since the image only covers bout 60% of the box
+    player_box.classList.add("pick-box-picked")
+    // cant just switch bg images since the image should cover bout 60% of the box, theres prolly better way
     const brawler_img = new Image(width, height);
     brawler_img.src = picked_brawler_img_src
     brawler_img.style.position= "absolute";
@@ -50,11 +55,27 @@ function choose_brawler(brawler){
     brawler_img.style.transform= "translateX(-50%)";
     brawler_img.style.zIndex=1;
     brawler_img.style.paddingBottom = "3vh";
+    brawler_img.id = "brawler-img"
     player_box.appendChild(brawler_img);
     padding = Math.abs(brawler_img.width-width)
     // for some reason i cant change the size before loading and if i do it onload it looks weird when zoomed (idk why either its off by like 1 pixel) so i add adequate padding and it JUST WORKS
     brawler_img.style.paddingRight = padding/2
     brawler_img.style.paddingLeft = padding/2
+  }
+  // if all brawlers are picked the next brawler click just removes all the picks for now, maybe ill change that into a reset button or sth
+  else{
+    console.log('why are the frogs gay?')
+
+    while (document.getElementById("brawler-img")){
+      img = document.getElementById("brawler-img")
+      img.remove();
+      $('.pick-box-picked').removeClass('pick-box-picked');
+      $('.brawler-picked').css('opacity', 1)
+      $('.brawler-picked').removeClass('brawler-picked');
+      pick_number = 1
+    }
+
+
   }
 }
 
