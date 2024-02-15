a6e2dd167302d456187f6fcdeabc16d5f8f19ea7
Iamthemarcin <Iamthemarcin@wp.pl>
2024-02-15 17:17:53 +0100

    power league map list done

--- homepage/views.py
+++ homepage/views.py
@@ -31,38 +31,46 @@ def update_map_list(): #allright, so there isnt any way to get the current power
 #     were played in the competetive mode. Then just go through maps in those games and add them to a set. after that i should have all the possible power league maps.
     path = '{}/map_list.txt'.format(settings.STATICFILES_DIRS[0])
     headers = {
-        'Authorization': "Bearer: eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiIsImtpZCI6IjI4YTMxOGY3LTAwMDAtYTFlYi03ZmExLTJjNzQzM2M2Y2NhNSJ9.eyJpc3MiOiJzdXBlcmNlbGwiLCJhdWQiOiJzdXBlcmNlbGw6Z2FtZWFwaSIsImp0aSI6IjQ1N2E1NTY2LTU0ZmItNDViZC1iYTFlLWQ3MDAyM2NmYzViNSIsImlhdCI6MTcwNzc2NjI1OSwic3ViIjoiZGV2ZWxvcGVyLzQ5MzI1NGU4LTQ1YTQtNjViYy1hMGEyLTI3ZmM0ZjQ4NWZhZiIsInNjb3BlcyI6WyJicmF3bHN0YXJzIl0sImxpbWl0cyI6W3sidGllciI6ImRldmVsb3Blci9zaWx2ZXIiLCJ0eXBlIjoidGhyb3R0bGluZyJ9LHsiY2lkcnMiOlsiODUuNzYuMTQxLjE0NCJdLCJ0eXBlIjoiY2xpZW50In1dfQ.FEx62IsImNPFpkjDKet4lOYQTteKFUo1eDpUPvMiObOWbGI27DgdwRhzzbCTj258Ib7wxptKdcLIFppMOJd6PQ"
+        'Authorization': "Bearer: eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiIsImtpZCI6IjI4YTMxOGY3LTAwMDAtYTFlYi03ZmExLTJjNzQzM2M2Y2NhNSJ9.eyJpc3MiOiJzdXBlcmNlbGwiLCJhdWQiOiJzdXBlcmNlbGw6Z2FtZWFwaSIsImp0aSI6IjY1NGM2MzQyLWUxMGQtNDhhNC04NzcwLWM4ZDBkZGRjNmY3NSIsImlhdCI6MTcwODAwNjQ4MCwic3ViIjoiZGV2ZWxvcGVyLzQ5MzI1NGU4LTQ1YTQtNjViYy1hMGEyLTI3ZmM0ZjQ4NWZhZiIsInNjb3BlcyI6WyJicmF3bHN0YXJzIl0sImxpbWl0cyI6W3sidGllciI6ImRldmVsb3Blci9zaWx2ZXIiLCJ0eXBlIjoidGhyb3R0bGluZyJ9LHsiY2lkcnMiOlsiODQuMjQ5LjEwLjEzMiJdLCJ0eXBlIjoiY2xpZW50In1dfQ.9fY5kKDoBHKg4AE_thJDjK107U9wiMVlXgu08UUz9_lrvz43fD5ro-B714FHFCve0g3-mUbSBupsuyh7LNzcRg"
     }
+    map_dict = {}
+
     top_players = requests.get('https://api.brawlstars.com/v1/rankings/global/players?limit=50', headers)
     top_players = top_players.json()
     top_players_tags = []
     search_response(top_players,'tag', None, top_players_tags)
-    
-    all_games = requests.get('https://api.brawlstars.com/v1/players/%23PQ28JP0P/battlelog', headers)
-    all_games = all_games.json()
-    print(all_games)
-    def look_for_ranked_games(game_data):
-        for battles in game_data['items']:
-            print(battles)
-            if battles['event']['type']:
-                print(battles['event']['type'])
-    look_for_ranked_games(all_games)
 
+    def look_for_ranked_games(game_data, map_dict):
+        if not game_data['items']:
+            return print("No games retrieved")
+        for battles in game_data['items']:         
+            if battles['battle']:
+                battle_type = battles['battle']['type']      
+                if battle_type == 'soloRanked' or battle_type == 'teamRanked':
+                    
+                    ranked_game_map = str(battles['event']['map'])
+                    ranked_game_mode = str(battles['battle']['mode'])
+                    if ranked_game_mode in map_dict:
+                        map_dict[ranked_game_mode].add(ranked_game_map)
+                    else:
+                        map_dict[ranked_game_mode] = {ranked_game_map}
 
-        
+    for player in top_players_tags:
 
+        player = player.replace('#', '')
+        request_link = 'https://api.brawlstars.com/v1/players/%23{}/battlelog'.format(player)
+        all_games = requests.get(request_link, headers)
+        all_games = all_games.json()
+        look_for_ranked_games(all_games, map_dict)
 
-
-"""
     map_list = open(path, 'w')
-    map_list.write('{}'.format(top_players_tags))
+    map_list.write('{}'.format(map_dict))
     map_list.close()
-"""
-update_map_list()
-
 
 def index(request):
+    #update_map_list()  whenever a power league update comes use this function XD
     path = '{}/images/brawlers/'.format(settings.STATICFILES_DIRS[0])
+    map_path = '{}/map_list.txt'.format(settings.STATICFILES_DIRS[0])
     img_list = os.listdir(path)
     half = len(img_list)//2
     top_row = img_list[:half]
@@ -70,11 +78,18 @@ def index(request):
     events_request = requests.get('https://api.brawlapi.com/v1/events')
     events_json = events_request.json()
     
+    #choose a random map and mode
+
+    map_list = open(path, 'w')
+    map_list.write('{}'.format(map_dict))
+    map_list.close()
+
+
     game_modes = ['Brawl-Ball', 'Gem-Grab', 'Heist', 'Knockout', 'Wipeout', 'Hot-Zone'] #gotta get those from somewhere else future me, i aint updating this by hand
     chosen_mode = random.choice(game_modes)
     mode_icon_link = []
 
-    #find the icon for the gamemode, could also just make a list and save them locally every season,not sure, ill put it in a function in case i wanna do that.
+    # find the icon for the gamemode, could also just make a list and save them locally every season,not sure, ill put it in a function in case i wanna do that.
     search_response(events_json, 'imageUrl', chosen_mode, mode_icon_link)
     chosen_mode = chosen_mode.replace('-',' ')
 
