b0670cf72158b237fc74b29a17e14226c6bd2b0c
Iamthemarcin <Iamthemarcin@wp.pl>
2024-02-23 13:06:11 +0100

    database runnin, time to make models  n stuff

--- power_draft/settings.py
+++ power_draft/settings.py
@@ -75,10 +75,23 @@ WSGI_APPLICATION = 'power_draft.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
+
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+
+        'NAME': 'power_draft',
+
+        'USER': 'marcin',
+
+        'PASSWORD': 'otyhaker',
+
+        'HOST': 'localhost',
+
+        'PORT': '5432',
+
     }
+
 }
 
 
