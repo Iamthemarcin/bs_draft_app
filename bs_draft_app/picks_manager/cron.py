7277f0eb07240f47c7184257f22df8601be0cad7
Iamthemarcin <Iamthemarcin@wp.pl>
2024-06-07 14:21:39 +0200

    prepping for deploying on AWS

--- bs_draft_app/picks_manager/cron.py
+++ bs_draft_app/picks_manager/cron.py
@@ -1,7 +1,7 @@
 from .views import ManageDB 
 
 def update_db():
-    scan_ammount = 3
+    scan_ammount = 20
     Manager = ManageDB()
     scanned_games = Manager.update_map_list_and_winrate(scan_ammount)
     print(f"scanned {scanned_games} ranked games")
