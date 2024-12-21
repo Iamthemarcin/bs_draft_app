0953684c4c144165a62614b01ea492f465d27c1f
iamthemarcin <iamthemarcin@wp.pl>
2024-12-21 14:22:31 +0100

    update_db command fix

--- bs_draft_app/picks_manager/management/commands/update_db.py
+++ bs_draft_app/picks_manager/management/commands/update_db.py
@@ -1,15 +1,13 @@
 from django.core.management.base import BaseCommand, CommandError
 # created this as a command to debug my cronjob easier
 from picks_manager.views import ManageDB
-from picks_manager.models import ScannedData, Brawler
+from picks_manager.models import ScannedData
 
 
 def update_db(scan_ammount):
     Manager = ManageDB()
-    all_brawlers = Brawler.objects.all()
-    objects = {'brawlers': all_brawlers}
-    scanned_games, objects = Manager.update_map_list_and_winrate(
-        scan_ammount, debug=True, objects=objects)
+    scanned_games = Manager.update_map_list_and_winrate(
+        scan_ammount, debug=True)
     print(f"scanned {scanned_games} ranked games")
     scan_data = ScannedData.objects.first()
     scan_data.scanned_games += scanned_games
